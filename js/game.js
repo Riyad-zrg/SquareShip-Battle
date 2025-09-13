4c3a6f34a8fa45bee76ac3f2ffa00f281d5b6627
zerr0009 <riyad.zerrougui@etudiant.univ-reims.fr>
2025-09-13 20:44:05 +0200

    setup constant speed for obstacles

--- js/game.js
+++ js/game.js
@@ -54,8 +54,8 @@ function create() {
         callback: () => {
             let x = Phaser.Math.Between(20, 380);
             let obstacle = this.physics.add.sprite(x, -50, 'obstacleTex');
-            obstacle.body.setAllowGravity(true);
-            obstacle.body.setGravityY(500);
+            obstacle.body.setAllowGravity(false);
+            obstacle.body.setVelocityY(400); //Speed of obstacles
             obstacles.push(obstacle);
         },
         loop: true
